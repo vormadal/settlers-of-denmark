0a0ca0d2282e57ed023cede9b7a9335ffb89c2cb
Rúni Vørmadal <rev@ski.dk>
2024-10-18 23:04:57 +0200

    added tile to frontend and other minor stuff

--- sod-client/src/App.tsx
+++ sod-client/src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layer, Stage, Star, Text, RegularPolygon } from "react-konva";
+import "./App.css";
+import { Land } from "./shapes/Land";
+import { LandTiles } from "./models/LandTiles";
+import { Position } from "./models/Position";
 
+const tiles = [
+  "Dessert",
+  "Forrest",
+  "Mountains",
+  "Lifestock",
+  "Mine",
+  "Grain",
+].map(
+  (x, i) => new LandTiles(`tile-${i}`, x, new Position(100+ i * 100, 100), [], [])
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        {tiles.map((x) => (
+          <Land key={x.id} tile={x} />
+        ))}
+        {/* <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+          />
+        ))} */}
+      </Layer>
+    </Stage>
   );
 }
 
---
+++ sod-client/src/shapes/Land.tsx
@@ -0,0 +1,28 @@
+import { RegularPolygon } from "react-konva";
+import { LandTiles } from "../models/LandTiles";
+
+interface Type {
+  tile: LandTiles;
+}
+
+const colors: { [key: string]: string } = {
+  Dessert: "#d9bf65",
+  Forrest: "#008000",
+  Mountains: "#708090",
+  Lifestock: "#adff2f",
+  Mine: "#d2691e",
+  Grain: "#ffdf00",
+};
+
+export function Land({ tile }: Type) {
+  return (
+    <RegularPolygon
+      radius={60}
+      sides={6}
+      rotation={90}
+      x={tile.position.x}
+      y={tile.position.y}
+      fill={colors[tile.type] ?? "#ff0000"}
+    />
+  );
+}
