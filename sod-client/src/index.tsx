68af9d8dea96c9a2d7057ef369e79277a1193f4c
Rúni Vørmadal <rev@ski.dk>
2024-10-19 00:11:51 +0200

    render server state

--- sod-client/src/index.tsx
+++ sod-client/src/index.tsx
@@ -3,33 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as Colyseus from "colyseus.js"; // not necessary if included via <script> tag.
-
-var client = new Colyseus.Client('ws://localhost:2567');
-client.joinOrCreate("my_room").then(room => {
-  console.log(room.sessionId, "joined", room.name);
-
-  room.onStateChange((state) => {
-    console.log(room.name, "has new state:", state);
-  });
-
-  room.onMessage("message_type", (message) => {
-    console.log(room.sessionId, "received on", room.name, message);
-  });
-
-  room.onError((code, message) => {
-    console.log(room.sessionId, "couldn't join", room.name);
-  });
-
-  room.onLeave((code) => {
-    console.log(room.sessionId, "left", room.name);
-  });
-
-}).catch(e => {
-  console.log("JOIN ERROR", e);
-});
-
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- sod-client/src/models/Position.ts
+++
@@ -1,3 +0,0 @@
-export class Position {
-  constructor(public readonly x: number, public readonly y: number) {}
-}
