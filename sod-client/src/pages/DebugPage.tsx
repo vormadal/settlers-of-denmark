d73dda841612d2ef5b4fee0dda0cc3db470f4393
Rúni Vørmadal <runivormadal@gmail.com>
2024-11-10 22:16:13 +0100

    feat: automate debug view for faster debugging

--- sod-client/src/pages/DebugPage.tsx
+++ sod-client/src/pages/DebugPage.tsx
@@ -1,7 +1,7 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, TextField } from '@mui/material'
 import { Board } from '../Board'
 import { useColyseus } from '../context/ColyseusContext'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useGameState } from '../context/GameStateContext'
 import { GameState } from '../state/GameState'
 import { PlayerInformation } from '../PlayerInformation'
@@ -9,29 +9,24 @@ import { useMyPlayer } from '../context/MeContext'
 
 const sidebarWidth = 300
 function DebugPage() {
+  const [numPlayers, setNumPlayers] = useState(2)
   const client = useColyseus()
-  const [state, room, setRoom] = useGameState()
-  const [me, setMe] = useMyPlayer()
+  const [state, , setRoom] = useGameState()
+  const [, setMe] = useMyPlayer()
 
   const newGame = useCallback(async () => {
     const room = await client.create<GameState>('debug')
     setRoom(room)
-  }, [client, setRoom])
-
-  const addPlayer = useCallback(async () => {
-    room?.send('addPlayer')
-  }, [room])
-
-  const startGame = useCallback(async () => {
+    for (let i = 1; i < numPlayers; i++) {
+      room.send('addPlayer')
+    }
     room?.send('startGame')
-  }, [room])
+  }, [client, setRoom, numPlayers])
 
-  const changeTurn = useCallback(
-    async (id: string) => {
-      setMe(id)
-    },
-    [setMe]
-  )
+  useEffect(() => {
+    if (!state) return
+    setMe(state.currentPlayer)
+  }, [state, setMe])
 
   useEffect(() => {
     newGame()
@@ -40,40 +35,22 @@ function DebugPage() {
   return (
     <Box sx={{ width: '100%', background: '#7CB3FF', display: 'flex' }}>
       <Box sx={{ width: sidebarWidth, background: '#fff' }}>
-        <Button
-          onClick={newGame}
-          variant="contained"
-          color="primary"
-        >
-          New Game
-        </Button>
-
-        <Button
-          onClick={addPlayer}
-          variant="contained"
-          color="primary"
-        >
-          Add Player
-        </Button>
-
-        <Button
-          onClick={startGame}
-          variant="contained"
-          color="primary"
-        >
-          Start Game
-        </Button>
-        {[...(state?.players.values() || [])].map((x) => (
+        <Box sx={{ gap: 1, display: 'flex', flexDirection: 'column', padding: 1 }}>
+          <TextField
+            name="numPlayers"
+            label="Number of Players"
+            type="number"
+            value={numPlayers}
+            onChange={(e) => setNumPlayers(Number(e.target.value))}
+          />
           <Button
-            key={x.id}
-            onClick={() => changeTurn(x.id)}
+            onClick={newGame}
+            variant="contained"
+            color="primary"
           >
-            Change Turn ({x.id})
+            New Game
           </Button>
-        ))}
-        <p>
-            Me: {me?.id}
-        </p>
+        </Box>
         <PlayerInformation width={sidebarWidth} />
       </Box>
       <Board width={window.innerWidth - sidebarWidth} />
