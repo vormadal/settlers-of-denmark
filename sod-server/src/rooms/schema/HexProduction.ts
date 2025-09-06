c94bedc5e3c914dad295f660fc1fcf699ee28e11
Rúni Vørmadal <runivormadal@gmail.com>
2025-09-07 00:10:58 +0200

    fix: city only receiving one resource card

--- sod-server/src/rooms/schema/HexProduction.ts
+++ sod-server/src/rooms/schema/HexProduction.ts
@@ -1,33 +1,48 @@
-import { ArraySchema, Schema, type } from '@colyseus/schema'
-import { GameState } from './GameState'
-import { Card, CardTypes } from './Card'
+import { ArraySchema, Schema, type } from "@colyseus/schema";
+import { GameState } from "./GameState";
+import { Card, CardTypes } from "./Card";
 
 export class HexProduction extends Schema {
-  @type('string') hexType: string
-  @type('string') structureType: string
-  @type(['string']) resourceTypes = new ArraySchema<string>()
+  @type("string") hexType: string;
+  @type("string") structureType: string;
+  @type(["string"]) resourceTypes = new ArraySchema<string>();
   // resource or valuables (gold, etc), base game only has resource
-  @type('string') cardType: string
+  @type("string") cardType: string;
 
   getResources(state: GameState) {
-    const resources: Card[] = []
-    // maybe this logic should be in a command
-    for (const resource of this.resourceTypes) {
-      const availableCards = state.deck.filter((x) => !x.owner && x.type === this.cardType && resource === x.variant)
-      if (availableCards.length > 0) {
-        resources.push(availableCards[0])
-      }
+    const resources: Card[] = [];
+
+    const resourceCount = this.resourceTypes.reduce((acc, curr) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    }, {} as Record<string, number>);
+
+    for (const resource of Object.keys(resourceCount)) {
+      const availableCards = state.deck.filter(
+        (x) => !x.owner && x.type === this.cardType && resource === x.variant
+      );
+
+      resources.push(
+        ...availableCards.slice(
+          0,
+          Math.min(resourceCount[resource], availableCards.length)
+        )
+      );
     }
 
-    return resources
+    return resources;
   }
 
-  static createResource(hexType: string, structureType: string, ...resourceTypes: string[]) {
+  static createResource(
+    hexType: string,
+    structureType: string,
+    ...resourceTypes: string[]
+  ) {
     return new HexProduction().assign({
       hexType,
       structureType,
       resourceTypes,
-      cardType: CardTypes.Resource
-    })
+      cardType: CardTypes.Resource,
+    });
   }
 }
