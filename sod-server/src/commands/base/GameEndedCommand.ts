d7e764adbc119d705777374399e4547db8b50772
Eivind Egholm Vørmadal <eev_98@hotmail.com>
2025-09-12 18:02:08 +0200

    refactor: removed log

--- sod-server/src/commands/base/GameEndedCommand.ts
+++ sod-server/src/commands/base/GameEndedCommand.ts
@@ -3,7 +3,6 @@ import { MyRoom } from '../../rooms/MyRoom'
 
 export class GameEndedCommand extends Command<MyRoom> {
   execute() {
-    console.log("Game Ended")
     this.state.isGameEnded = true
   }
 }
