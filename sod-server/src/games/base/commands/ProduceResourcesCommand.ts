afac92e8a68c847fcf90b83fd3fb040d94438e1f
Rúni Vørmadal <runivormadal@gmail.com>
2025-10-20 22:44:56 +0200

    fix: type issues on server

--- sod-server/src/games/base/commands/ProduceResourcesCommand.ts
+++ sod-server/src/games/base/commands/ProduceResourcesCommand.ts
@@ -1,47 +1,38 @@
-import { Command } from "@colyseus/command";
-import { MyRoom } from "../../../rooms/MyRoom";
-import { DieTypes } from "../../../rooms/schema/Die";
+import { Command } from '@colyseus/command'
+import { MyRoom } from '../../../rooms/MyRoom'
+import { DieTypes } from '../../../rooms/schema/Die'
 
 export class ProduceResourcesCommand extends Command<MyRoom> {
   execute() {
-    const diceRoll = this.state.dice
-      .filter((x) => x.type === DieTypes.Regular)
-      .reduce((sum, die) => sum + die.value, 0);
+    const diceRoll = this.state.dice.filter((x) => x.type === DieTypes.Regular).reduce((sum, die) => sum + die.value, 0)
     // robber
     if (diceRoll === 7) {
-      return;
+      return
     }
 
-    const hexes = this.state.hexes.filter((x) => x.value === diceRoll && x.id !== this.state.robberHex);
+    const hexes = this.state.hexes.filter((x) => x.value === diceRoll && x.id !== this.state.robberHex)
 
-    const currentPlayer = this.state.players.get(this.state.currentPlayer);
-    const currentPlayerIndex = this.state.playerList.indexOf(currentPlayer);
+    const currentPlayer = this.state.players.get(this.state.currentPlayer)
+    const currentPlayerIndex = this.state.playerList.indexOf(currentPlayer)
 
     for (let i = 0; i < this.state.playerList.length; i++) {
       // in case the bank runs out of resources, we need to give resources in the correct order
       // starting from the current player then looping through the rest of the players
-      const player =
-        this.state.playerList[
-          (i + currentPlayerIndex) % this.state.playerList.length
-        ];
+      const player = this.state.playerList[(i + currentPlayerIndex) % this.state.playerList.length]
 
       for (const hex of hexes) {
         const structures = this.state.structures.filter(
-          (x) =>
-            hex.intersections.includes(x.intersection) && x.owner === player.id
-        );
+          (x) => hex.intersections.find((i) => i.id === x.intersection) && x.owner === player.id
+        )
 
         for (const structure of structures) {
           const resources =
             this.state.hexProductions
-              .find(
-                (x) =>
-                  x.hexType === hex.type && x.structureType === structure.type
-              )
-              ?.getResources(this.state) || [];
+              .find((x) => x.hexType === hex.type && x.structureType === structure.type)
+              ?.getResources(this.state) || []
 
           for (const resource of resources) {
-            resource.owner = structure.owner;
+            resource.owner = structure.owner
           }
         }
       }
